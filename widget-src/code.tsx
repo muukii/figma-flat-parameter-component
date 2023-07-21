b521212847b1d0b2275ad62ac2ff4d725386558e
Muukii <muukii.app@gmail.com>
2023-07-21 13:21:44 +0900

    :evergreen_tree: Update

--- widget-src/code.tsx
+++ widget-src/code.tsx
@@ -1,5 +1,5 @@
 const { widget } = figma;
-const { Frame, AutoLayout, useSyncedState, Input, Text } = widget;
+const { Frame, AutoLayout, useSyncedState, Input, Text, Ellipse } = widget;
 
 function FlatParameterView(props: {
   data: Data;
@@ -8,45 +8,69 @@ function FlatParameterView(props: {
   return (
     <AutoLayout direction="vertical">
       {props.data.parameters.map((parameter, index) => (
-        <AutoLayout key={index} direction="horizontal">
-          <Input
-            value={parameter.identifier}
-            placeholder="name"
-            onTextEditEnd={(value) => {
-              const parameters = [...props.data.parameters];
-              parameters[index] = {
-                identifier: value.characters,
-                value: parameter.value,
-              };
-              props.onChange({ ...props.data, parameters });
-            }}
-          />
-          <Input
-            value={parameter.value}
-            placeholder="value"
-            onTextEditEnd={(value) => {
-              const parameters = [...props.data.parameters];
-              parameters[index] = {
-                identifier: parameter.identifier,
-                value: value.characters,
-              };
-              props.onChange({ ...props.data, parameters });
-            }}
-          />
+        <AutoLayout direction="vertical">
+          <AutoLayout key={index} direction="horizontal">
+            <AutoLayout
+              key={index}
+              direction="horizontal"
+              verticalAlignItems="center"
+            >
+              <AutoLayout padding={8}>
+                <Ellipse width={4} height={4} fill={"#000000"} />
+              </AutoLayout>
+              <Input
+                value={parameter.identifier}
+                placeholder="name"
+                onTextEditEnd={(value) => {
+                  const parameters = [...props.data.parameters];
+                  parameters[index] = {
+                    identifier: value.characters,
+                    value: parameter.value,
+                  };
+                  props.onChange({ ...props.data, parameters });
+                }}
+              />
+            </AutoLayout>
+            <Input
+              value={parameter.value}
+              placeholder="value"
+              onTextEditEnd={(value) => {
+                const parameters = [...props.data.parameters];
+                parameters[index] = {
+                  identifier: parameter.identifier,
+                  value: value.characters,
+                };
+                props.onChange({ ...props.data, parameters });
+              }}
+            />
+          </AutoLayout>
+          <AutoLayout
+            padding={{ left: 8, right: 8, top: 16, bottom: 16 }}
+            width={"fill-parent"}
+          >
+            <Frame width={"fill-parent"} height={1} fill={"#F2F2F2"} />
+          </AutoLayout>
         </AutoLayout>
       ))}
-      <Text
-        onClick={() => {
-          const parameters = [...props.data.parameters];
-          parameters.push({
-            identifier: "",
-            value: "",
-          });
-          props.onChange({ ...props.data, parameters });
-        }}
+      <AutoLayout
+        width={"fill-parent"}
+        direction="horizontal"
+        padding={{ top: 8, bottom: 8 }}
       >
-        Add
-      </Text>
+        <AutoLayout width={"fill-parent"} height={1} />
+        <Text
+          onClick={() => {
+            const parameters = [...props.data.parameters];
+            parameters.push({
+              identifier: "",
+              value: "",
+            });
+            props.onChange({ ...props.data, parameters });
+          }}
+        >
+          Add
+        </Text>
+      </AutoLayout>
     </AutoLayout>
   );
 }
@@ -66,7 +90,14 @@ function Widget() {
   });
 
   return (
-    <AutoLayout direction="vertical">
+    <AutoLayout
+      direction="vertical"
+      fill={"#FFFFFF"}
+      stroke={"#F2F2F2"}
+      strokeWidth={2}
+      padding={16}
+      cornerRadius={8}
+    >
       <FlatParameterView data={data} onChange={setData} />
     </AutoLayout>
   );
